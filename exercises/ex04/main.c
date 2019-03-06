fb7f8278cbb38e274d3fffe2f36cbca366752694
Ilya Besancon <ilya.besancon@students.olin.edu>
2019-03-05 19:06:48 -0500

    Exercise 04

---
+++ exercises/ex04/main.c
@@ -0,0 +1,92 @@
+#include "trout.h"
+#include "util.h"
+//Fixing some errors:
+#include <unistd.h>
+unsigned int alarm(unsigned int seconds);
+
+#include <string.h>
+
+#include <sys/types.h>
+uid_t getuid(void);
+uid_t geteuid(void);
+
+int setuid(uid_t uid);
+
+pid_t getpid(void);
+pid_t getppid(void);
+
+/* variables we might want to configure */
+int max_ttl = 30;
+int nprobes = 2;
+
+/* other global variables */
+
+int seq = 0;
+
+char recvbuf[BUFSIZE];
+char sendbuf[BUFSIZE];
+Rec *rec = (Rec *) sendbuf;
+
+int sendfd, recvfd;
+int pipefd[2];              /* the pipe for the alarm handler */
+
+Sockaddr *sasend;    /* socket addresses for various purposes */
+Sockaddr *sarecv;
+Sockaddr *salast;
+Sockaddr *sabind;
+
+socklen_t salen;                    /* length of a socket address */
+int datalen = sizeof (Rec);         /* length of the data in a datagram */
+
+u_short sport;                      /* source UDP port # */
+u_short dport = 32768 + 668;        /* destination port -- hopefully unused */
+                                    /* 668 = the neighbor of the beast */
+Timeval sendtv[1];
+Timeval recvtv[1];
+Timeval difftv[1];
+
+
+
+int main (int argc, char **argv)
+{
+  int c;
+  struct addrinfo *ai;
+  char *host;
+
+  opterr = 0;
+  while ( (c = getopt (argc, argv, "m:")) != -1) {
+    switch (c) {
+    case 'm':
+      if ( (max_ttl = atoi(optarg)) <= 1) {
+	err_quit ("invalid -m value");
+      }
+      break;
+    default:
+      err_quit ("unrecognizd option: %c", c);
+    }
+  }
+
+  if (optind != argc - 1) {
+    err_quit ("usage: trout [ -m <maxttl>] <hostname>");
+  }
+  host = argv[optind];
+  ai = Host_serv (host, NULL, 0, 0);
+
+  printf ("trout to %s (%s): %d hops max, %d data bytes\n",
+	  ai->ai_canonname,
+	  Sock_ntop_host (ai->ai_addr, ai->ai_addrlen),
+	  max_ttl, datalen);
+
+  if (ai->ai_family != AF_INET) {
+    err_quit ("unknown address family %d", ai->ai_family);
+  }
+
+  sasend = ai->ai_addr;
+  salen = ai->ai_addrlen;
+  sarecv = Calloc (1, salen);
+  salast = Calloc (1, salen);
+  sabind = Calloc (1, salen);
+
+  loop_ttl ();
+  exit (0);
+}
