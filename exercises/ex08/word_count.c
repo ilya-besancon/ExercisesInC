18c4b6ab619ec3270ca1f096399f06d69548e099
Ilya Besancon <ilya.besancon@students.olin.edu>
2019-04-09 21:11:08 -0400

    pushing ex08 initial changes

---
+++ exercises/ex08/word_count.c
@@ -0,0 +1,22 @@
+/*
+Created by Ilya Besancon
+
+The purpose of this program is to read a text file,
+split lines into words, and count the frequency of each word.
+
+You should use Glib types, functions and data structures wherever
+possible (and appropriate).  You should check the return values of all
+function calls and handle errors appropriately.  You don't have to
+free your data structures; we will work on that in a future exercise.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
+
+
+int main(){
+  printf("Yay, the program runs!\n");
+  return 0;
+}
